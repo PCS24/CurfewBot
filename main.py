7c0f4b0b5c78ae53338aab87e86f83499021a1fe
builderpepc <troy.hiruka@gmail.com>
2022-01-30 15:42:36 -0500

    chore: borrowed base code from FattahBot

---
+++ main.py
@@ -0,0 +1,88 @@
+import traceback
+import discord
+from discord.ext import commands
+import asyncio
+import logging
+import os
+import utils
+import discord_emoji
+from dotenv import load_dotenv
+import datetime
+import sys
+
+load_dotenv()
+
+# Change working directory
+ROOT_PATH = utils.getRootPath()
+os.chdir(ROOT_PATH)
+
+# Get config data
+CONFIG = utils.getConfig('Static/config.template_yaml', 'Config/config.yaml')
+
+# Configure logging
+LOG_PATH = 'Logs/' + datetime.datetime.now().strftime("%m-%d-%Y_%H%M%S") + ".log"
+log_level_switch = (
+    {
+        "DEBUG": logging.DEBUG,
+        "INFO": logging.INFO,
+        "WARN": logging.WARNING,
+        "WARNING": logging.WARNING,
+        "ERROR": logging.ERROR,
+        "CRITICAL": logging.CRITICAL
+    }
+)
+logging.basicConfig(level=log_level_switch[CONFIG['Logging']['logging_level'].upper()], format='%(asctime)s %(levelname)s %(name)s | %(message)s', datefmt=CONFIG['Logging']['date_format'])
+logger = logging.getLogger()
+fileHandler = logging.FileHandler(LOG_PATH)
+fileHandler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s %(name)s | %(message)s', datefmt=CONFIG['Logging']['date_format']))
+if CONFIG['Logging']['log_files']:
+    logger.addHandler(fileHandler)
+logging.info("Config loaded, logger started")
+logging.info("Python " + sys.version.split(" ")[0])
+logging.info("Discord.py version " + discord.__version__)
+
+if CONFIG['Bot']['token'] == 'env':
+    if not 'DISCORD_TOKEN' in os.environ.keys():
+        logging.critical("Token set to 'env' in config but env variable 'DISCORD_TOKEN' was not found. Quitting.")
+        sys.exit()
+    else:
+        CONFIG['Bot']['token'] = os.environ['DISCORD_TOKEN']
+
+for p in CONFIG['Bot']['placeholders']:
+    CONFIG['Bot']['placeholders'][p] = discord_emoji.to_unicode(CONFIG['Bot']['placeholders'][p])
+
+# Create bot instance
+bot = utils.CurfewBot(CONFIG, command_prefix=utils.getPrefix, intents=discord.Intents.all(), case_insensitive=True)
+#bot.remove_command('help') # Remove default help command
+
+# Load cogs
+# Add cog paths each time one is created
+COGS = []
+
+for cog in COGS:
+    try:
+        bot.load_extension(cog)
+    except:
+        logging.exception("Failed to load cog '" + cog + "'")
+        raise
+    else:
+        logging.info("Loaded cog '" + cog + "'")
+
+@bot.event
+async def on_command_error(ctx: commands.Context, error: commands.CommandError):
+    error = getattr(error, "original", error)
+    if ctx.command != None:
+        logging.exception(f"'{type(error)}' exception occurred while executing command \'{ctx.command.name}\': {error}")
+
+@bot.event
+async def on_ready():
+    logging.info("CurfewBot online")
+
+if bot.config['Bot']['jishaku']:
+    bot.load_extension('jishaku')
+    logging.info("Loaded jishaku")
+try:
+    bot.run(bot.config['Bot']['token'])
+except discord.LoginFailure:
+    logging.critical("Could not log in! Exiting. Check token in config?", exc_info=True)
+    sys.exit()
---
+++ utils.py
@@ -0,0 +1,75 @@
+import os
+from ruamel.yaml import YAML
+yaml = YAML()
+from typing import Sequence, List, Iterable, Set, Union
+import discord
+from discord.ext import commands
+
+def getRootPath() -> Union[os.PathLike, str]:
+    """
+    Convenience function. Gets the root path of the bot.
+    """
+    return os.path.dirname(os.path.realpath(__file__))
+
+os.chdir(getRootPath())
+
+def genFromTemplate(template_path: os.PathLike, target_path: os.PathLike):
+    """
+    Detects if a config file is missing, and generates it from its template if it is.
+    """
+    if not os.path.exists(target_path):
+        with open(template_path, 'rb') as template_file:
+            template = template_file.read()
+
+        with open(target_path, 'wb') as f:
+            f.write(template)
+
+def updateConfig(template_path: os.PathLike, target_path: os.PathLike):
+    """
+    Detects if a config file is outdated by comparing it to its template, and then recursively updates it if it is.
+    """
+    template_data = yaml.load(open(template_path, 'r'))
+    target_data = yaml.load(open(target_path, 'r'))
+    if template_data['Metadata']['VERSION'] > target_data['Metadata']['VERSION']:
+        # Template is newer than target, update target file
+        new_data = copy.copy(template_data)
+
+        target_data['Metadata']['VERSION'] = template_data['Metadata']['VERSION']
+
+        def updateAttributes(thing1, thing2):
+            for x in thing2.keys():
+                if not hasattr(thing2[x], 'keys'):
+                    thing1[x] = thing2[x]
+                else:
+                    updateAttributes(thing1[x], thing2[x])
+            
+            return thing1
+
+        new_data = updateAttributes(new_data, target_data)
+        yaml.dump(new_data, open(target_path, 'w'))
+
+        return new_data
+
+    else:
+        # Target is not outdated, return existing data
+        return target_data
+
+def getConfig(template_path: os.PathLike, target_path: os.PathLike):
+    """
+    Convenience function. Generates the config (YAML) from template if it does not exist and updates it if it does exist and is outdated.
+    """
+    genFromTemplate(template_path, target_path)
+    return updateConfig(template_path, target_path)
+
+class CurfewBot(commands.Bot):
+
+    def __init__(self, config, *args, **kwargs):
+        super(CurfewBot, self).__init__(*args, **kwargs)
+        self.config = config
+
+def getPrefix(bot: CurfewBot, message: discord.Message) -> str:
+    """
+    When a prefix-change command is implemented for external servers, this function will get the custom prefix
+    """
+    return bot.config['Bot']['default_prefix']
+
