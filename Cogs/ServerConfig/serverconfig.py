c6654eac4f7013f90672b1726f218e5b801d73ae
builderpepc <troy.hiruka@gmail.com>
2022-02-06 21:41:40 -0500

    feat: ignored channels list commands

--- Cogs/ServerConfig/serverconfig.py
+++ Cogs/ServerConfig/serverconfig.py
@@ -7,11 +7,36 @@ from discord.commands.context import ApplicationContext
 import utils
 import logging
 import aiosqlite
+from typing import Coroutine, Any, Union, Callable
 
 logger = logging.getLogger('cog-serverconfig')
 
 NAME = "Config"
 DESCRIPTION = "Commands for configuring the bot in your server."
+
+# Functions that are reused in both RolesGroup and ChannelsGroup
+async def list_objects(ctx: ApplicationContext, name: str, fetch_func: Callable, color_key: str = 'primary'):
+    objects = await fetch_func(ctx.guild)
+    embed = discord.Embed(
+        title=(' '.join([n.capitalize() for n in name.split(' ')]) + " List"),
+        description=('\n'.join([o.mention for o in objects]) if len(objects) > 0 else f"*No {name}s found.*"),
+        color=ctx.bot.getColor(color_key)
+    )
+    await ctx.respond(embed=embed, ephemeral=True)
+
+async def edit_object_list(ctx: ApplicationContext, name: str, fetch_func: Callable, appending: bool, value: Union[discord.Role, discord.abc.GuildChannel], column: str):
+    db = await ctx.bot.connect_db()
+    try:
+        objects = await fetch_func(ctx.guild, db=db)
+        if ((value in objects) and appending) or ((value not in objects) and not appending):
+            await ctx.respond(f"{ctx.bot.getPlaceholder('error')} The {name} {value.mention} is {'already' if appending else 'not'} in the list.", ephemeral=True)
+            return
+        (objects.append if appending else objects.remove)(value)
+        await db.execute(f"UPDATE GUILD_SETTINGS SET \"{column}\"=? WHERE GUILD_ID=?", (','.join([str(o.id) for o in objects]), ctx.guild.id))
+        await db.commit()
+    finally:
+        await db.close()
+    await ctx.respond(f"{'Added' if appending else 'Removed'} {name} {value.mention} {'to' if appending else 'from'} the list.", ephemeral=True)
 
 class ServerConfigCog(commands.Cog, name=NAME, description=DESCRIPTION):
     def __init__(self, bot: utils.CurfewBot):
@@ -31,43 +56,35 @@ class ServerConfigCog(commands.Cog, name=NAME, description=DESCRIPTION):
 
                         @self.command(name="list", description="Lists all roles that are affected by lockdowns and reopenings.")
                         async def list_roles(ctx: ApplicationContext):
-                            roles = await ctx.bot.get_target_roles(ctx.guild)
-                            embed = discord.Embed(
-                                title="Role List",
-                                description=('\n'.join([r.mention for r in roles]) if len(roles) > 0 else "*No roles found.*"),
-                                color=ctx.bot.getColor('primary')
-                            )
-                            await ctx.respond(embed=embed, ephemeral=True)
+                            await list_objects(ctx, 'role', ctx.bot.get_target_roles)
 
                         @self.command(name="add", description="Adds a role to the list of roles that are affected by lockdowns and reopenings.")
                         async def add_role(ctx: ApplicationContext, role: discord.Role):
-                            db = await ctx.bot.connect_db()
-                            try:
-                                roles = await ctx.bot.get_target_roles(ctx.guild, db=db)
-                                if role in roles:
-                                    await ctx.respond(f"{ctx.bot.getPlaceholder('error')} The role {role.mention} is already in the list.", ephemeral=True)
-                                    return
-                                roles.append(role)
-                                await db.execute("UPDATE GUILD_SETTINGS SET TARGET_ROLES=? WHERE GUILD_ID=?", (','.join([str(r.id) for r in roles]), ctx.guild.id))
-                                await db.commit()
-                            finally:
-                                await db.close()
-                            await ctx.respond(f"Added role {role.mention} to the list.", ephemeral=True)
+                            await edit_object_list(ctx, 'role', ctx.bot.get_target_roles, True, role, "TARGET_ROLES")
 
                         @self.command(name="remove", description="Removes a role from the list of roles that are affected by lockdowns and reopenings.")
                         async def remove_role(ctx: ApplicationContext, role: discord.Role):
-                            db = await ctx.bot.connect_db()
-                            try:
-                                roles = await ctx.bot.get_target_roles(ctx.guild, db=db)
-                                if role not in roles:
-                                    await ctx.respond(f"{ctx.bot.getPlaceholder('error')} The role {role.mention} is not in the list.", ephemeral=True)
-                                    return
-                                roles.remove(role)
-                                await db.execute("UPDATE GUILD_SETTINGS SET TARGET_ROLES=? WHERE GUILD_ID=?", (','.join([str(r.id) for r in roles]), ctx.guild.id))
-                                await db.commit()
-                            finally:
-                                await db.close()
-                            await ctx.respond(f"Removed role {role.mention} from the list.", ephemeral=True)
+                            await edit_object_list(ctx, 'role', ctx.bot.get_target_roles, False, role, "TARGET_ROLES")
+
+                @self.subgroup("channels", "Manage the list of channels that are ignored by lockdowns and reopenings.")
+                class ChannelsGroup(discord.SlashCommandGroup):
+                    def __init__(self, *args, **kwargs):
+                        super(type(self), self).__init__(*args, **kwargs)
+
+                        channel_option = discord.Option((discord.TextChannel, discord.VoiceChannel, discord.CategoryChannel, discord.StageChannel), "The channel/category. Category permissions may sync to ignored channels if already synced.")
+
+                        @self.command(name="list", description="Lists all ignored channels.")
+                        async def list_roles(ctx: ApplicationContext):
+                            await list_objects(ctx, 'ignored channel', ctx.bot.get_ignored_channels)
+
+                        @self.command(name="ignore", description="Adds a channel to the list of ignored channels.")
+                        async def ignore_channel(ctx: ApplicationContext, channel: channel_option):
+                            await edit_object_list(ctx, 'channel', ctx.bot.get_ignored_channels, True, channel, "IGNORED_CHANNELS")
+
+                        @self.command(name="unignore", description="Removes a channel from the list of ignored channels.")
+                        async def unignore_channel(ctx: ApplicationContext, channel: channel_option):
+                            await edit_object_list(ctx, 'channel', ctx.bot.get_ignored_channels, False, channel, "IGNORED_CHANNELS")
+
 
 def setup(bot: utils.CurfewBot):
     bot.add_cog(ServerConfigCog(bot))
