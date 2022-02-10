07105b66fd065617839e5f37203431fbfbb54608
builderpepc <troy.hiruka@gmail.com>
2022-02-10 00:06:54 -0500

    fix: add missing ephemeral=True

--- Cogs/ServerConfig/serverconfig.py
+++ Cogs/ServerConfig/serverconfig.py
@@ -94,7 +94,7 @@ class ServerConfigCog(commands.Cog, name=NAME, description=DESCRIPTION):
                         async def set_log_channel(ctx: ApplicationContext, channel: discord.TextChannel):
                             channel_perms: discord.Permissions = channel.permissions_for(ctx.guild.me)
                             if not (channel_perms.view_channel and channel_perms.send_messages and channel_perms.use_external_emojis and channel_perms.embed_links and channel_perms.attach_files):
-                                await ctx.respond(f"{ctx.bot.getPlaceholder('error')} Please make sure I have all of the following permissions in {channel.mention} first: `View Channel`, `Send Messages`, `Embed Links`, `Attach Files`, and `Use External Emojis`.")
+                                await ctx.respond(f"{ctx.bot.getPlaceholder('error')} Please make sure I have all of the following permissions in {channel.mention} first: `View Channel`, `Send Messages`, `Embed Links`, `Attach Files`, and `Use External Emojis`.", ephemeral=True)
                                 return
                             db = await ctx.bot.connect_db()
                             try:
